884a392b322f4081d1ce84bda629bab01aec9c0a
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-01-28 22:53:21 +0900

    add/activation_patterns_detection

---
+++ activated_neuron/new_neurons/activation_patterns/act_pattarns.py
@@ -0,0 +1,116 @@
+import os
+import sys
+sys.path.append("/home/s2410121/proj_LA/activated_neuron/new_neurons/activation_patterns")
+import random
+import dill as pickle
+
+import numpy as np
+import torch
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from datasets import load_dataset
+
+from funcs import (
+    get_act_patterns,
+    get_act_patterns_with_edit_activation,
+    activation_patterns_lineplot,
+    save_as_pickle,
+    unfreeze_pickle,
+)
+
+# L1 = english
+L1 = "en"
+""" model configs """
+# LLaMA-3
+model_names = {
+    # "base": "meta-llama/Meta-Llama-3-8B"
+    "ja": "tokyotech-llm/Llama-3-Swallow-8B-v0.1", # ja
+    # "de": "DiscoResearch/Llama3-German-8B", # ger
+    "nl": "ReBatch/Llama-3-8B-dutch", # du
+    "it": "DeepMount00/Llama-3-8b-Ita", # ita
+    "ko": "beomi/Llama-3-KoEn-8B", # ko
+}
+""" parameters """
+activation_type = "abs"
+# activation_type = "product"
+norm_type = "no"
+# n_list = [100, 1000, 3000, 4000, 5000, 7000, 10000, 15000, 20000, 30000] # patterns of intervention_num
+n_list = [15000]
+
+for L2, model_name in model_names.items():
+    """ get curpus and models """
+
+    """ tatoeba translation corpus """
+    dataset = load_dataset("tatoeba", lang1=L1, lang2=L2, split="train")
+    num_sentences = 2000
+    dataset = dataset.select(range(num_sentences))
+    tatoeba_data = []
+    for item in dataset:
+        # check if there are empty sentences.
+        if item['translation'][L1] != '' and item['translation'][L2] != '':
+            tatoeba_data.append((item['translation'][L1], item['translation'][L2]))
+    # tatoeba_data = [(item['translation'][L1], item['translation'][L2]) for item in dataset]
+    tatoeba_data_len = len(tatoeba_data)
+    """
+    baseとして、対訳関係のない1文ずつのペアを作成
+    (L1(en)はhttps://huggingface.co/datasets/agentlans/high-quality-english-sentences,
+    L2はtatoebaの該当データを使用)
+    """
+    random_data = []
+    # L1(en)
+    en_base_ds = load_dataset("agentlans/high-quality-english-sentences")
+    random_data_en = en_base_ds["train"][:num_sentences]
+    en_base_ds_idx = 0
+    for item in dataset:
+        random_data.append((random_data_en["text"][en_base_ds_idx], item["translation"][L2]))
+        en_base_ds_idx += 1
+
+    """ model and device configs """
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model_name = model_names[L2]
+    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    """
+    get act_pattenrs as cos_sim (with no intervention).
+    """
+    # get activation list
+    act_patterns = get_act_patterns(model, tokenizer, device, tatoeba_data)
+    act_patterns_baseline = get_act_patterns(model, tokenizer, device, random_data)
+    # plot activation patterns.
+    activation_patterns_lineplot(act_patterns, act_patterns_baseline, L2, activation_type, "no")
+
+    """
+    get act_patterns as cos_sim (with high AP neurons intervention).
+    """
+    # unfreeze AP_list.
+    pkl_file_path = f"/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/AUC/act_{activation_type}/ap_scores/{norm_type}_norm/sorted_neurons_{L2}.pkl"
+    sorted_neurons_AP = unfreeze_pickle(pkl_file_path)
+
+    for n in n_list:
+        """ どのくらい介入するか(n) """
+        intervention_num = n
+        sorted_neurons_AP_main = sorted_neurons_AP[:intervention_num]
+        # baseline
+        sorted_neurons_AP_baseline = random.sample(sorted_neurons_AP[intervention_num+1:], len(sorted_neurons_AP[intervention_num+1:]))
+        sorted_neurons_AP_baseline = sorted_neurons_AP_baseline[:intervention_num]
+
+        """ deactivate high AP neurons. """
+        # get activation list
+        act_patterns = get_act_patterns_with_edit_activation(model, tokenizer, device, sorted_neurons_AP_main, tatoeba_data)
+        act_patterns_baseline = get_act_patterns_with_edit_activation(model, tokenizer, device, sorted_neurons_main, random_data)
+        # plot activation patterns.
+        activation_patterns_lineplot(act_patterns, act_patterns_baseline, L2, activation_type, "yes")
+
+        """ deactivate baseline neurons. """
+        # get activation list
+        act_patterns = get_act_patterns_with_edit_activation(model, tokenizer, device, sorted_neurons_AP_baseline, tatoeba_data)
+        act_patterns_baseline = get_act_patterns_with_edit_activation(model, tokenizer, device, sorted_neurons_AP_baseline, random_data)
+        # plot activation patterns.
+        activation_patterns_lineplot(act_patterns, act_patterns_baseline, L2, activation_type, "baseline")
+
+        print(f"intervention_num: {n} <- completed.")
+
+    # delete model and some cache
+    del model
+    torch.cuda.empty_cache()
---
+++ activated_neuron/new_neurons/activation_patterns/funcs.py
@@ -0,0 +1,176 @@
+import os
+import random
+import sys
+import pickle
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import transformers
+from baukit import TraceDict
+from datasets import get_dataset_config_names, load_dataset
+from sklearn.metrics.pairwise import cosine_similarity
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+""" func for editing activation values """
+def edit_activation(output, layer, layer_idx_and_neuron_idx):
+    """
+    edit activation value of neurons(indexed layer_idx and neuron_idx)
+    output: activation values
+    layer: sth like 'model.layers.{layer_idx}.mlp.act_fn'
+    layer_idx_and_neuron_idx: list of tuples like [(layer_idx, neuron_idx), ....]
+    """
+    for layer_idx, neuron_idx in layer_idx_and_neuron_idx:
+        if str(layer_idx) in layer:  # layer名にlayer_idxが含まれているか確認
+            output[:, :, neuron_idx] *= 0  # 指定されたニューロンの活性化値をゼロに設定
+
+    return output
+
+def get_act_patterns_with_edit_activation(model, tokenizer, device, layer_neuron_list, data):
+    trace_layers = [f'model.layers.{layer}.mlp.act_fn' for layer, _ in layer_neuron_list]
+    with TraceDict(model, trace_layers, edit_output=lambda output, layer: edit_activation(output, layer, layer_neuron_list)) as tr:
+        return get_act_patterns(model, tokenizer, device, data)
+
+def get_act_patterns(model, tokenizer, device, data):
+
+    # layers_num
+    num_layers = 32 if model_name == "llama" else 12
+    # nums of total neurons (per a layer)
+    num_neurons = 14336
+
+    """
+    activation valuesを保存する dict (shared neuronsが対象)
+    {
+        layer_idx: [cos_sim1, cos_sim2, ....] <- act_pattern(cos_sim of act_values): list
+    }
+    """
+    act_patterns_dict = {}
+
+    # Track neurons with tatoeba
+    for L1_text, L2_text in data:
+        """
+        get activation values
+        mlp_activation_L1/L2: [torch.Tensor(batch_size, sequence_length, num_neurons) * num_layers]
+        """
+        # L1 text
+        input_ids_L1 = tokenizer(L1_text, return_tensors="pt").input_ids.to(device)
+        token_len_L1 = len(input_ids_L1[0])
+        act_fn_value_L1, up_proj_value_L1 = act_llama3(model, input_ids_L1)
+
+        # L2 text
+        input_ids_L2 = tokenizer(L2_text, return_tensors="pt").input_ids.to(device)
+        token_len_L2 = len(input_ids_L2[0])
+        act_fn_value_L2, up_proj_value_L2 = act_llama3(model, input_ids_L2)
+        """
+        neurons(in llama3 MLP): up_proj(x) * act_fn(gate_proj(x)) <- input to down_proj()
+        L1/L2 shared neurons
+        """
+        for layer_idx in range(len(act_fn_value_L1)):
+            """ consider last token only """
+            # L1
+            act_fn_value_L1[layer_idx] = act_fn_value_L1[layer_idx][:, token_len_L1 - 1, :]
+            up_proj_value_L1[layer_idx] = up_proj_value_L1[layer_idx][:, token_len_L1 - 1, :]
+            # L2
+            act_fn_value_L2[layer_idx] = act_fn_value_L2[layer_idx][:, token_len_L2 - 1, :]
+            up_proj_value_L2[layer_idx] = up_proj_value_L2[layer_idx][:, token_len_L2 - 1, :]
+            """ get neuron activation values: up_proj(x) * act_fn(x) """
+            neurons_L1_values = calc_element_wise_product(act_fn_value_L1[layer_idx], up_proj_value_L1[layer_idx]) # torch.Tensor
+            neurons_L2_values = calc_element_wise_product(act_fn_value_L2[layer_idx], up_proj_value_L2[layer_idx])
+            """ calc act_patterns (cos_sim). """ 
+            act_patterns = cosine_similarity(neurons_L1_values, neurons_L2_values)[0, 0]
+            act_patterns_dict[layer_idx].append(act_patterns)
+
+    return act_patterns_dict
+
+
+def activation_patterns_lineplot(act_patterns, act_patterns_baseline, L2, activation_type, intervention="no"):
+    """
+    Plots line plots of cosine similarity means(Activation Patterns) per each layer for two dictionaries(main and baseline),
+    with variance (standard deviation) as a shaded region.
+
+    Parameters:
+    - act_patterns: Dictionary in the format {layer_idx: [cos_sim1, cos_sim2, ...]}
+    - act_patterns_baseline: Dictionary in the format {layer_idx: [cos_sim1, cos_sim2, ...]}
+    """
+    # Get all unique layer indices
+    all_layer_idxs = [i for i in range(32)]
+
+    def compute_stats(data_dict, layers):
+        """Compute mean and standard deviation for each layer index."""
+        means, std_devs = [], []
+        for layer in layers:
+            values = data_dict.get(layer)
+            means.append(np.mean(values))
+            std_devs.append(np.std(values))
+        return np.array(means), np.array(std_devs)
+
+    # Compute mean and standard deviation for each dictionary
+    means1, std_devs1 = compute_stats(act_patterns, all_layer_idxs)
+    means2, std_devs2 = compute_stats(act_patterns_baseline, all_layer_idxs) # baseline
+
+    # Plot
+    plt.figure(figsize=(12, 6))
+    
+    # Plot act_patterns
+    plt.plot(all_layer_idxs, means1, marker='o', linestyle='-', color='blue', label="Same Semantics")
+    plt.fill_between(all_layer_idxs, means1 - std_devs1, means1 + std_devs1, color='blue', alpha=0.2, label="Same_S Std Dev")
+
+    # Plot baseline
+    plt.plot(all_layer_idxs, means2, marker='s', linestyle='-', color='red', label="Different Semantics")
+    plt.fill_between(all_layer_idxs, means2 - std_devs2, means2 + std_devs2, color='red', alpha=0.2, label="diff_S Std Dev")
+
+    # Labels and title
+    plt.xlabel("Layer Index", fontsize=35)
+    plt.ylabel("Cosine Sim", fontsize=35)
+    plt.tick_params(axis='x', labelsize=15)
+    plt.tick_params(axis='y', labelsize=15)
+    plt.title(f"en_{L2}")
+    plt.legend()
+    plt.grid(True)
+    # save_path
+    if intervention == "no":
+        save_path = f"/home/s2410121/proj_LA/activated_neuron/new_neurons/images/act_patterns/llama3/act_{activation_type}/{L2}.png"
+    elif intervention == "yes":
+        save_path = f"/home/s2410121/proj_LA/activated_neuron/new_neurons/images/act_patterns/llama3/act_{activation_type}/intervention/{L2}.png"
+    elif intervention == "baseline":
+        save_path = f"/home/s2410121/proj_LA/activated_neuron/new_neurons/images/act_patterns/llama3/act_{activation_type}/intervention_baseline/{L2}.png"
+    
+    plt.savefig(
+        save_path,
+        bbox_inches="tight"
+        )
+    plt.close()
+
+def get_activation_value(activations, neuron_idx):
+    """
+    get activation vlaue of neuron_idx.
+    """
+    # 指定された層、トークン、ニューロンの発火値を取得
+    activation_value = activations[0][neuron_idx].item()
+
+    return activation_value
+
+def get_activation_value_abs(activations, neuron_idx):
+    """
+    get activation vlaue of neuron_idx.
+    """
+    activation_value = abs(activations[0][neuron_idx].item())
+
+    return activation_value
+
+def save_as_pickle(file_path, target_dict) -> None:
+    """
+    save dict as pickle file.
+    """
+    # directoryを作成（存在しない場合のみ)
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "wb") as f:
+        pickle.dump(target_dict, f)
+
+def unfreeze_pickle(file_path: str) -> dict:
+    """
+    unfreeze pickle file as dict.
+    """
+    with open(file_path, "rb") as f:
+        return_dict = pickle.load(f)
+    return return_dict
