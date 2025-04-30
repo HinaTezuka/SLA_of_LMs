13cf7b60370f3a082f7f47920002d50181498bb3
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-05-01 00:28:15 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/logistic_regression/visualization.py
+++ activated_neuron/new_neurons/transfer_neurons/logistic_regression/visualization.py
@@ -18,7 +18,7 @@ for model_type in model_types:
         for score in layer_scores:
             acc_matrix[i, score['layer']] = np.mean(score['accuracy'])
 
-    # plot
+    # figure設定（大きめに）
     plt.figure(figsize=(15, 6))
     ax = sns.heatmap(
         acc_matrix,
@@ -26,8 +26,9 @@ for model_type in model_types:
         fmt=".2f",
         cmap='Blues',
         xticklabels=range(1, num_layers + 1),
-        yticklabels=[f"en-{l}" for l in langs],
-        cbar_kws={'label': 'Test Accuracy'},
+        yticklabels=[f"en-{l}" for l in langs] if model_type == 'aya' else [],
+        cbar=(model_type == 'aya'),
+        cbar_kws={'label': 'Test Accuracy'} if model_type == 'aya' else None,
         linewidths=0.5,
         linecolor='white',
         annot_kws={"size": 10},
@@ -36,15 +37,17 @@ for model_type in model_types:
 
     title = 'LLaMA3-8B' if model_type == 'llama3' else 'Mistral-7B' if model_type == 'mistral' else 'Aya expanse-8B'
     ax.set_title(title, fontsize=20)
-    if model_type == 'llama3':
+
+    if model_type == 'aya':
         ax.set_xlabel("Layer Index", fontsize=15)
         ax.set_ylabel("Language Pair", fontsize=15)
-    plt.yticks(fontsize=10)
-    plt.xticks(fontsize=10)
-
-    # ラベルを横向きに
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
-    ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=0, fontsize=10)
+        ax.set_yticklabels(ax.get_yticklabels(), rotation=0, fontsize=10)
+    else:
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
 
     plt.tight_layout()
     save_path = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/images/transfers/logistic_regression/{model_type}.png'
