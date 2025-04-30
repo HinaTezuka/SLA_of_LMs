d9c068d8bf7a72d6f01307a15cc2932df282fa3c
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-05-01 00:36:14 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/logistic_regression/visualization.py
+++ activated_neuron/new_neurons/transfer_neurons/logistic_regression/visualization.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import os
+from matplotlib import gridspec
 
 model_types = ['llama3', 'mistral', 'aya']
 langs = ['ja', 'nl', 'ko', 'it']
@@ -18,9 +19,17 @@ for model_type in model_types:
         for score in layer_scores:
             acc_matrix[i, score['layer']] = np.mean(score['accuracy'])
 
-    # figure設定（大きめに）
-    plt.figure(figsize=(15, 6))
-    ax = sns.heatmap(
+    if model_type == 'aya':
+        fig = plt.figure(figsize=(15, 6.2))  # 高さ調整
+        gs = gridspec.GridSpec(2, 1, height_ratios=[25, 1], hspace=0.01)  # ここを最小に
+        ax = fig.add_subplot(gs[0])
+        cbar_ax = fig.add_subplot(gs[1])
+    else:
+        plt.figure(figsize=(15, 6))
+        ax = plt.gca()
+        cbar_ax = None
+
+    sns.heatmap(
         acc_matrix,
         annot=True,
         fmt=".2f",
@@ -28,11 +37,13 @@ for model_type in model_types:
         xticklabels=range(1, num_layers + 1),
         yticklabels=[f"en-{l}" for l in langs] if model_type == 'aya' else [],
         cbar=(model_type == 'aya'),
-        cbar_kws={'label': 'Test Accuracy'} if model_type == 'aya' else None,
+        cbar_ax=cbar_ax if model_type == 'aya' else None,
+        cbar_kws={'orientation': 'horizontal', 'label': 'Test Accuracy'} if model_type == 'aya' else None,
         linewidths=0.5,
         linecolor='white',
         annot_kws={"size": 10},
         square=True,
+        ax=ax
     )
 
     title = 'LLaMA3-8B' if model_type == 'llama3' else 'Mistral-7B' if model_type == 'mistral' else 'Aya expanse-8B'
@@ -43,13 +54,14 @@ for model_type in model_types:
         ax.set_ylabel("Language Pair", fontsize=15)
         ax.set_xticklabels(ax.get_xticklabels(), rotation=0, fontsize=10)
         ax.set_yticklabels(ax.get_yticklabels(), rotation=0, fontsize=10)
+        fig.subplots_adjust(top=0.94, bottom=0.13, left=0.08, right=0.98)  # 上下余白を少し狭める
     else:
         ax.set_xlabel("")
         ax.set_ylabel("")
         ax.set_xticklabels([])
         ax.set_yticklabels([])
+        plt.tight_layout()
 
-    plt.tight_layout()
     save_path = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/images/transfers/logistic_regression/{model_type}.png'
     plt.savefig(save_path, bbox_inches='tight')
     plt.close()
