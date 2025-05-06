240a151db72f39be3746c1d183296055c5e28a8a
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-05-06 17:52:21 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/qa/mkqa_all_questions.py
+++ activated_neuron/new_neurons/transfer_neurons/qa/mkqa_all_questions.py
@@ -18,7 +18,7 @@ from qa_funcs import (
 )
 
 # load models.
-model_names = ['meta-llama/Meta-Llama-3-8B', 'mistralai/Mistral-7B-v0.3', 'CohereForAI/aya-expanse-8b']
+# model_names = ['meta-llama/Meta-Llama-3-8B', 'mistralai/Mistral-7B-v0.3', 'CohereForAI/aya-expanse-8b']
 model_names = ['mistralai/Mistral-7B-v0.3']
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 langs = ['ja', 'nl', 'ko', 'it', 'en']
@@ -67,7 +67,7 @@ for model_name in model_names:
 
         # intervention baseline.
         random.seed(42)
-        intervened_neurons_baseline = random.sample(intervened_neurons[intervention_num+1:], len(intervened_neurons[intervention_num+1:]))
+        intervened_neurons_baseline = random.sample(intervened_neurons[intervention_num:], intervention_num)
         intervened_neurons_baseline = intervened_neurons_baseline[:intervention_num]
         result_score_baseline = mkqa_all_with_edit_activation(model, tokenizer, device, qa, L2, intervened_neurons_baseline)
         path_intervention_baseline = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/{model_type}/qa/intervention_n{intervention_num}/all_questions_baseline_{L2}.pkl'
