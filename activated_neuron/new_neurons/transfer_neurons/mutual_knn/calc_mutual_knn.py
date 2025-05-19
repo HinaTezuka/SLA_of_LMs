5a12e27bd09a4c17ea1a087563ea8e13f6849908
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-05-20 00:56:44 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/mutual_knn/calc_mutual_knn.py
+++ activated_neuron/new_neurons/transfer_neurons/mutual_knn/calc_mutual_knn.py
@@ -28,25 +28,25 @@ langs = ['ja', 'nl', 'ko', 'it']
 L1 = 'en'
 topk = 5 # number of nearest neighbor.
 
-for model_name in model_names:
-    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model_type = 'llama3' if 'llama' in model_name else 'mistral' if 'mistral' in model_name else 'aya'
-    knn_scores = {}
-    for L2 in langs:
-        path = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/sentence_data/{L2}_multi_train.pkl'
-        sentences = unfreeze_pickle(path)
-        res = compute_mutual_knn(model, tokenizer, device, sentences, L1, L2, topk=topk) # res: [knn_score_layer1, knn_score_layer2, ...]
-        print(f'=================={model_type}, {L2}==================')
-        print(res)
-        knn_scores[L2] = res
+# for model_name in model_names:
+#     model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+#     tokenizer = AutoTokenizer.from_pretrained(model_name)
+#     model_type = 'llama3' if 'llama' in model_name else 'mistral' if 'mistral' in model_name else 'aya'
+#     knn_scores = {}
+#     for L2 in langs:
+#         path = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/sentence_data/{L2}_multi_train.pkl'
+#         sentences = unfreeze_pickle(path)
+#         res = compute_mutual_knn(model, tokenizer, device, sentences, L1, L2, topk=topk) # res: [knn_score_layer1, knn_score_layer2, ...]
+#         print(f'=================={model_type}, {L2}==================')
+#         print(res)
+#         knn_scores[L2] = res
     
-    path = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/{model_type}/knn/res_all_langs_k{topk}.pkl'
-    save_as_pickle(path, knn_scores)
+#     path = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/{model_type}/knn/res_all_langs_k{topk}.pkl'
+#     save_as_pickle(path, knn_scores)
 
-    # clear cache.
-    del model
-    torch.cuda.empty_cache()
+#     # clear cache.
+#     del model
+#     torch.cuda.empty_cache()
 
 """ visualization. """
 model_types = ['llama3', 'mistral', 'aya']
@@ -78,11 +78,11 @@ for model_type in model_types:
     subset = df[df['Model'] == model_type]
     sns.lineplot(data=subset, x='Layer', y='Mutual KNN', hue='L2', palette='tab10', linewidth=3)
     model_name = 'LLaMA3-8B' if model_type == 'llama3' else 'Mistral-7B' if model_type == 'mistral' else 'Aya-expanse-8B'
-    plt.title(f'{model_name}', fontsize=30)
-    plt.xlabel('Layer Index', fontsize=35)
-    plt.ylabel('Mutual KNN', fontsize=35)
+    plt.title(f'{model_name}', fontsize=45)
+    plt.xlabel('Layer Index', fontsize=40)
+    plt.ylabel('Mutual KNN', fontsize=40)
     plt.ylim(0, 0.6)
-    plt.tick_params(axis='both', labelsize=15)
+    plt.tick_params(axis='both', labelsize=25)
     plt.grid(True)
     plt.tight_layout()
     plt.legend(title='L2', fontsize=15, title_fontsize=15)
