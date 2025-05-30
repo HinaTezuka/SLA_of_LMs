b334363778b0aa28b5f8df22a06a295268dda167
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-05-30 09:57:19 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/subspace/svd.py
+++ activated_neuron/new_neurons/transfer_neurons/subspace/svd.py
@@ -21,7 +21,7 @@ langs = ["ja", "nl", "ko", "it", "en"]
 # LLaMA3-8B / Mistral-7B / Aya-expanse-8B.
 model_names = ["meta-llama/Meta-Llama-3-8B", "mistralai/Mistral-7B-v0.3", 'CohereForAI/aya-expanse-8b']
 threshold_log = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))  # ← これを追加
-is_scaled = True
+is_scaled = False
 
 for model_name in model_names:
     model_type = 'llama3' if 'llama' in model_name else 'mistral' if 'mistral' in model_name else 'aya'
