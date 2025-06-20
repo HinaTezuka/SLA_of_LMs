8aa7b0f50b282dc2730188117f2a633a66a1c036
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-06-20 09:29:18 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/qa/mkqa_all_questions.py
+++ activated_neuron/new_neurons/transfer_neurons/qa/mkqa_all_questions.py
@@ -20,8 +20,8 @@ from qa_funcs import (
 # load models.
 model_names = ['mistralai/Mistral-7B-v0.3', 'CohereForAI/aya-expanse-8b']
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# langs = ['ja', 'nl', 'ko', 'it', 'en']
-langs = ['vi', 'ru', 'fr']
+# langs = ['ja', 'nl', 'ko', 'it', 'en', 'vi']
+langs = ['ru', 'fr']
 """ 
 QA dataset: 
 MKQA: Multilingual Open Domain Question Answering
@@ -43,13 +43,13 @@ for model_name in model_names:
     model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    for L2 in langs:
-        # normal
-        result_scores = mkqa_all(model, tokenizer, device, qa, L2)
-        # save results as pkl.
-        path_normal = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/{model_type}/qa/all_questions_normal_{L2}.pkl'
-        save_as_pickle(path_normal, result_scores)
-        print(f'saved: normal: {model_type}, {L2}')
+    # for L2 in langs:
+    #     # normal
+    #     result_scores = mkqa_all(model, tokenizer, device, qa, L2)
+    #     # save results as pkl.
+    #     path_normal = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/{model_type}/qa/all_questions_normal_{L2}.pkl'
+    #     save_as_pickle(path_normal, result_scores)
+    #     print(f'saved: normal: {model_type}, {L2}')
 
     for L2 in langs:
         """ intervention """
