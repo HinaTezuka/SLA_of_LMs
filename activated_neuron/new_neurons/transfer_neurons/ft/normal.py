c75cedfdb8331c0c1ebeaa6e7588b83aea52f439
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-06-22 16:36:52 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/ft/normal.py
+++ activated_neuron/new_neurons/transfer_neurons/ft/normal.py
@@ -8,8 +8,20 @@ import torch
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+from custom_trainer import (
+    Trainer,
+)
+
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
+    # parser = ArgumentParser()
 
-    parser.add_argument('')
+    # parser.add_argument('')
+
+    """ test """
+    model_name = 'meta-llama/Meta-Llama-3-8B'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+    print(model.model.layers[0].mlp.down_proj.weight)
+    print(model.model.layers[0].mlp.down_proj.weight.shape)
+    sys.exit()
