59611af33c8e112b35be0e7e8e0ecb44a91c8e1f
Hinata Tezuka <hinata.tezuka.foreign@gmail.com>
2025-06-30 21:31:55 +0900

    update

--- activated_neuron/new_neurons/transfer_neurons/fluency/grammar_cheker.py
+++ activated_neuron/new_neurons/transfer_neurons/fluency/grammar_cheker.py
@@ -3,34 +3,39 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from collections import defaultdict
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import numpy as np
+
 import language_tool_python
+from language_tool_python.utils import classify_matches
+import cld3
 
 from funcs import (
     save_as_pickle,
     unfreeze_pickle,
 )
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-import numpy as np
-
-model_types = ['aya', 'llama3', 'mistral']
+model_types = ['llama3', 'aya', 'mistral']
 langs = ['ja', 'fr']
-is_baselines = [True, False]
+is_baselines = [False, True]
 num_to_extract = 100
+
+def lang_detection(sentence: str) -> bool: # 対象言語か/そもそも文字列が言語か　を判別用.
+    return cld3.get_language(sentence)
 
 for model_type in model_types:
     for L2 in langs:
-        tool = language_tool_python.LanguageTool('ja')
+        tool = language_tool_python.LanguageTool(L2)
         for is_baseline in is_baselines:
             save_path = f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/mmluprox/{model_type}/{L2}.pkl' if not is_baseline else f'/home/s2410121/proj_LA/activated_neuron/new_neurons/pickles/transfer_neurons/mmluprox/{model_type}/{L2}_baseline.pkl'
             sentences = unfreeze_pickle(save_path)
-            cnt = 0
-            for sentence in sentences:
-                matches = tool.check(sentence)
-                print(matches)
-                sys.exit()
-            
+            cnt = 0 # number of sentences judged gramatically correct.
+            for sentence_i, sentence in enumerate(sentences):
+                pred_language = lang_detection(sentence)
+                if pred_language is not None and pred_language.is_reliable and pred_language.language == 'ja':
+                    matches = tool.check(sentence)
+                    if len(matches) == 0: cnt += 1
             """ print results. """
             print(f'------------ {model_type}, {L2}, {"Baseline" if is_baseline else "Type-1"} ------------')
             print(f'proportion: {cnt / num_to_extract}, {cnt}/{num_to_extract}')
